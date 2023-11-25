f436496d96ee90fbe7646198876787bf4efbc54c
Fakhri Mahendra <13521045@std.stei.itb.ac.id>
2023-11-25 07:01:46 +0700

    test: make test file

---
+++ test/test_tcp_manager.py
@@ -0,0 +1,14 @@
+from lib.connection import Connection
+from lib.tcp_manager import TCPManager
+
+def main():
+    connection = Connection("127.0.0.1", 8080)
+    tcp_manager = TCPManager(connection=connection)
+    tcp_manager.always_listen()
+    connection.close()
+
+
+
+
+if __name__ == '__main__':
+    main()
